5a16618d410921800e4e2e43a721781b172a5a0f
jasminex21 <jasminexu7003@icloud.com>
2024-07-08 01:11:07 -0500

    fixed prev/next line generation for easy/medium modes

--- gui/tayLyrics_v2.py
+++ gui/tayLyrics_v2.py
@@ -6,7 +6,17 @@ from server_tools.Lyrics import Lyrics
 
 # TODO: fix mistake in I Can Fix Him (No Really I Can) AND Mary's Song (Oh My My My)
 all_lyrics = pd.read_csv("/home/jasmine/OneDrive - The University of Texas at Austin/Personal Projects/tayLyrics_v2/tayLyrics/TAYLOR_LYRICS_JUN2024.csv")
-all_albums = np.unique(all_lyrics["album_name"])
+all_albums = ["Taylor Swift", 
+              "Fearless (Taylor's Version)",
+              "Speak Now (Taylor's Version)", 
+              "Red (Taylor's Version)", 
+              "1989 (Taylor's Version)", 
+              "reputation", 
+              "Lover", 
+              "folklore", 
+              "evermore", 
+              "Midnights", 
+              "THE TORTURED POETS DEPARTMENT"]
 mode_options = ["Easy (an entire section, e.g. chorus)", 
                 "Medium (2 lines)", 
                 "Hard (1 line)"]
@@ -16,6 +26,11 @@ mode_mapping = {"Easy (an entire section, e.g. chorus)": "Easy",
 points_mapping = {"Easy (an entire section, e.g. chorus)": 1, 
                  "Medium (2 lines)": 3, 
                  "Hard (1 line)": 5}
+hint_help = """You get 3 hints per round; Hint 1 gives the album, 
+               and Hint 2 and 3 give the next and previous line, 
+               respectively. Each hint deducts 1 point from your total."""
+gamemode_options = ["Casual (no lives)", 
+                    "Survival (with 3 lives)"]
 
 if "lyrics" not in st.session_state: 
     st.session_state.lyrics = Lyrics(data=all_lyrics)
@@ -37,6 +52,8 @@ if "show_lyrics" not in st.session_state:
     st.session_state.show_lyrics = False
 if "disable_guess_input" not in st.session_state: 
     st.session_state.disable_guess_input = False
+if "disable_giveup_btn" not in st.session_state: 
+    st.session_state.disable_giveup_btn = False
 if "next" not in st.session_state: 
     st.session_state.next = False
 if "disable_hint_btn" not in st.session_state: 
@@ -57,7 +74,12 @@ def guess_submitted():
 # to be called when "Next Round" button is clicked
 def new_round(mode): 
     st.session_state.hint_count = 0
+    st.session_state.round_count += 1
+    st.session_state.show_lyrics = True
+    st.session_state.next = False
     st.session_state.disable_guess_input = False
+    st.session_state.disable_hint_btn = False
+    st.session_state.disable_giveup_btn = False
 
     generated_lyrics = st.session_state.lyrics.generate(mode)
     correct_song = st.session_state.lyrics.get_track_name()
@@ -82,6 +104,7 @@ def next_round():
 
 def give_up(): 
     st.session_state.disable_guess_input = True
+    st.session_state.points -= 2
     container.error(f"The correct answer was {st.session_state.correct_song}, from the album {st.session_state.correct_album}", icon="🚨")
     container.button("Next round", on_click=next_round)
 
@@ -96,6 +119,20 @@ def add_hint():
         container.info(f'Hint 3: the previous line of this song is "{st.session_state.prev_line}"', icon="ℹ️")
         st.session_state.disable_hint_btn = True
 
+def answered_correctly(): 
+    container.success(f'That is correct! The answer is indeed {st.session_state.correct_song}, from the album {st.session_state.correct_album}. Well done!', icon="✅")
+    st.session_state.points += points_mapping[mode]
+    st.session_state.correct_rounds_count += 1
+    st.session_state.guess = None
+    st.session_state.disable_hint_btn = True
+    st.session_state.disable_giveup_btn = True
+
+    container.button("Next round", on_click=next_round)
+
+def answered_incorrectly(): 
+    container.error(f'"{st.session_state.guess}" is not correct. Please try again!', icon="🚨")
+    st.session_state.points -= 1
+
 # TODO: implement survival mode (w/ lives)
 
 st.title("Welcome to :sparkles:tayLyrics:sparkles:!")
@@ -103,57 +140,54 @@ st.title("Welcome to :sparkles:tayLyrics:sparkles:!")
 # SIDEBAR #
 with st.sidebar: 
     with st.form("game_settings"): 
-        mode = st.selectbox("Select a game mode", 
-                            options=mode_options)
+        mode = st.selectbox("Select a game difficulty", 
+                            options=mode_options, 
+                            disabled=st.session_state.disable_start_btn)
+        game_mode = st.selectbox("Select a game mode", 
+                                 options=gamemode_options,
+                                 disabled=st.session_state.disable_start_btn)
         with st.expander("Advanced options"):
             selected_albums = st.multiselect("Select albums to generate lyrics from", 
                                              options=all_albums, 
-                                             default=all_albums)
+                                             default=all_albums, 
+                                             disabled=st.session_state.disable_start_btn)
         # TODO: disable button until game ends
         start_btn = st.form_submit_button("Start new game", disabled=st.session_state.disable_start_btn, on_click=disable_start_button)
 
 # MAIN PANEL #
 if (start_btn) or st.session_state.next: 
-    # TODO: reset to False if given up
-    st.session_state.round_count += 1
-    st.session_state.show_lyrics = True
-    st.session_state.next = False
-    st.session_state.hint_count = 0
-    st.session_state.disable_hint_btn = False
     st.session_state.generated_lyrics, st.session_state.correct_song, st.session_state.correct_album, st.session_state.next_line, st.session_state.prev_line = new_round(mode=mode)
 
 container = st.container(border=True)
 with container: 
-    st.write(st.session_state.generated_lyrics, unsafe_allow_html=True)
     if st.session_state.show_lyrics:
+        st.write(st.session_state.generated_lyrics, unsafe_allow_html=True)
         st.text_input("Enter your guess", 
                 placeholder="e.g. Back to December or Shake it Off", 
                 key="widget", on_change=guess_submitted,
                 disabled=st.session_state.disable_guess_input)
-        col1, col2, col3, col4 = st.columns(4)
-        hint_btn = col1.button("Hint", on_click=add_hint, disabled=st.session_state.disable_hint_btn)
-        giveup_btn = st.button("Give up", on_click=give_up)
-        col4.button("End current game", on_click=end_current_game)
         if st.session_state.guess: 
-            # returns True if correct
             if st.session_state.lyrics.get_guess_feedback(st.session_state.guess): 
-                st.success(f'That is correct! The answer is indeed {st.session_state.correct_song}, from the album {st.session_state.correct_album}. Well done!', icon="✅")
-                st.session_state.points += points_mapping[mode]
-                st.session_state.correct_rounds_count += 1
-                st.session_state.guess = None
-                st.button("Next round", on_click=next_round)
+                answered_correctly()
             else: 
-                st.error(f'"{st.session_state.guess}" is not correct. Please try again!', icon="🚨")
-                st.session_state.points -= 2
+                answered_incorrectly()
+        col1, col2, col3, col4 = st.columns(4)
+        hint_btn = col1.button("Hint", on_click=add_hint, disabled=st.session_state.disable_hint_btn, 
+                               help=hint_help)
+        giveup_btn = st.button("Give up", on_click=give_up, disabled=st.session_state.disable_giveup_btn,
+                               help="2 points are deducted from your total if you give up.")
+        col4.button("End current game", on_click=end_current_game)
+    else: 
+        st.markdown('Click the "Start new game" button to start guessing!')
 
 st.sidebar.divider()
 with st.sidebar.container(border=True):
     st.markdown("### Game Statistics")
-    st.markdown(f"**{mode_mapping[mode]} mode, round {st.session_state.round_count}**")
+    st.markdown(f"**{mode_mapping[mode]} difficulty, round {st.session_state.round_count}**")
     st.markdown(f"* Total points: {st.session_state.points}")
     if st.session_state.round_count: 
         accuracy_pct = round((st.session_state.correct_rounds_count / st.session_state.round_count), 2) * 100
         st.markdown(f"* Accuracy: {st.session_state.correct_rounds_count}/{st.session_state.round_count} ({accuracy_pct}%)")
         possible_pct = round((st.session_state.points / st.session_state.round_count * points_mapping[mode]), 2) * 100
-        st.markdown(f"* Points out of total possible: {st.session_state.points}/{st.session_state.round_count * points_mapping[mode]} ({possible_pct}%)")
+        st.markdown(f"* :green[Points out of total possible: {st.session_state.points}/{st.session_state.round_count * points_mapping[mode]} ({possible_pct}%)]")
         
--- server_tools/Lyrics.py
+++ server_tools/Lyrics.py
@@ -12,46 +12,56 @@ class Lyrics():
     def __init__(self, data):
         self.data = data
         self.rand_num = None
+        self.start_line = None
+        self.end_line = None
+    
+    def set_random_seed(seed):
+        try: 
+            random.seed(int(seed))
+        except ValueError: 
+            return "Seed must be a valid integer, such as 21 or 2003."
 
     def generate(self, mode):
         self.rand_num = random.randint(0, self.data.shape[0])
         if mode == "Hard (1 line)": 
+            self.start_line = self.rand_num
+            self.end_line = self.rand_num
             return self.data["lyric"][self.rand_num]
         if mode == "Medium (2 lines)": 
             track_name = self.data["track_name"][self.rand_num]
             # check if next line is from same song
             if self.data["track_name"][self.rand_num + 1] == track_name: 
-                start = self.rand_num
-                end = self.rand_num + 1
+                self.start_line = self.rand_num
+                self.end_line = self.rand_num + 1
             else: 
-                start = self.rand_num - 1
-                end = self.rand_num
-            return "<br>".join(self.data["lyric"][start:end + 1].tolist())
+                self.start_line = self.rand_num - 1
+                self.end_line = self.rand_num
+            return "<br>".join(self.data["lyric"][self.start_line:self.end_line + 1].tolist())
         else: 
             rand_section = self.data["element"][self.rand_num]
 
-            start = self.rand_num
-            while self.data["element"][start - 1] == rand_section: 
-                start -= 1
-            end = self.rand_num
-            while self.data["element"][end + 1] == rand_section: 
-                end += 1
-            return "<br>".join(self.data["lyric"][start:end + 1].tolist())
+            self.start_line = self.rand_num
+            while self.data["element"][self.start_line - 1] == rand_section: 
+                self.start_line -= 1
+            self.end_line = self.rand_num
+            while self.data["element"][self.end_line + 1] == rand_section: 
+                self.end_line += 1
+            return "<br>".join(self.data["lyric"][self.start_line:self.end_line + 1].tolist())
     
     def get_track_name(self): 
         return self.data["track_name"][self.rand_num]
     
     def get_album_name(self): 
         return self.data["album_name"][self.rand_num]
-
+    
     def get_previous_line(self):
-        if self.data["track_name"][self.rand_num - 1] == self.get_track_name():
-            return self.data["lyric"][self.rand_num - 1]
+        if self.data["track_name"][self.start_line - 1] == self.get_track_name():
+            return self.data["lyric"][self.start_line - 1]
         return "N/A"
 
     def get_next_line(self):
-        if self.data["track_name"][self.rand_num + 1] == self.get_track_name():
-            return self.data["lyric"][self.rand_num + 1]
+        if self.data["track_name"][self.end_line + 1] == self.get_track_name():
+            return self.data["lyric"][self.end_line + 1]
         return "N/A"
     
     def get_guess_feedback(self, guess): 
