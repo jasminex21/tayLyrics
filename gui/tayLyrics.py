909d8ac7b5ee62636d0c895b99c73ef9e3b2f344
jasminex21 <jasminexu7003@icloud.com>
2024-07-21 21:14:37 -0500

    fixed issues w game over and added themes up to 1989

--- gui/tayLyrics.py
+++ gui/tayLyrics.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from server_tools.Lyrics import Lyrics
 
-# TODO: possibly add leaderboard? Only if all albums selected
+# TODO: possibly add leaderboard? Only if all albums selected tho. some sort of database
 
 # TODO: fix mistake in I Can Fix Him (No Really I Can) AND Mary's Song (Oh My My My)
 all_lyrics = pd.read_csv("/home/jasmine/tayLyrics_v2/tayLyrics/TAYLOR_LYRICS_JUN2024.csv")
@@ -12,7 +12,7 @@ all_albums = ["Taylor Swift",
               "Fearless (Taylor's Version)",
               "Speak Now (Taylor's Version)", 
               "Red (Taylor's Version)", 
-              "1989 (Taylor's Version)", 
+              "1989 (Taylor's Version)",  
               "reputation", 
               "Lover", 
               "folklore", 
@@ -33,6 +33,50 @@ hint_help = """You get 3 hints per round; Hint 1 gives the album,
                respectively. Each hint deducts 1 point from your total."""
 gamemode_options = ["Casual (no lives)", 
                     "Survival (with 3 lives)"]
+# TODO: preliminary theme names - add emojis and fonts later!
+themes = ["Debut", 
+          "Fearless", 
+          "Speak Now", 
+          "Red",
+          "1989", 
+          "reputation",
+          "Lover",
+          "folklore",
+          "evermore",
+          "Midnights",
+          "Tortured Poets"]
+theme_css = {
+    "Debut": {
+        "background_color": "#006767",
+        "button_color": "#597A5D", 
+        "inputs": "#4A786D", 
+        "text_color": "white"
+    },
+    "Fearless": {
+        "background_color": "#d9c78f", 
+        "button_color": "#bb8f67",
+        "inputs": "#c3b377",
+        "text_color": "black"
+    }, 
+    "Speak Now": {
+        "background_color": "#8B698E",
+        "button_color": "#86469C",
+        "inputs": "#BB9AB1",
+        "text_color": "white"
+    },
+    "Red": {
+        "background_color": "#7D2727",
+        "button_color": "#755B48",
+        "inputs": "#5F2020",
+        "text_color": "white"
+    },
+    "1989": {
+        "background_color": "#8A96AE",
+        "button_color": "#ccc4c2",
+        "inputs": "	#748CA9",
+        "text_color": "black"
+    }
+            }
 
 if "lyrics" not in st.session_state: 
     st.session_state.lyrics = Lyrics(data=all_lyrics)
@@ -76,9 +120,44 @@ if "streak" not in st.session_state:
     st.session_state.streak = 0
 if "streaks" not in st.session_state: 
     st.session_state.streaks = []
-if "disable_general" not in st.session_state: 
-    st.session_state.disable_general = False
+if "game_over_msg" not in st.session_state: 
+    st.session_state.game_over_msg = ""
 
+def apply_theme(selected_theme):
+    css = f"""
+    <style>
+    .stApp > header {{
+        background-color: transparent;
+    }}
+    .eczjsme16 {{ 
+        background: {selected_theme['background_color']};
+    }}
+    .stApp {{
+        background: {selected_theme['background_color']};
+        color: {selected_theme["text_color"]};
+    }}
+    button {{
+        background-color: {selected_theme['button_color']} !important;
+    }}
+    [data-baseweb="base-input"], .st-cr, .st-c5 {{
+        background-color: {selected_theme['inputs']} !important;
+        color: {selected_theme["text_color"]};
+        border-radius: 10px;
+    }}
+    p, ul, h3, h1 {{
+        font-weight: bolder;
+        color: {selected_theme["text_color"]};
+    }}
+    .st-cb {{
+        color: {selected_theme["text_color"]};
+    }}
+    [data-baseweb="tag"] {{
+        background: {selected_theme['button_color']} !important;
+        color: {selected_theme["text_color"]};
+    }}
+    </style>
+    """
+    st.markdown(css, unsafe_allow_html=True)
 
 def guess_submitted():
     st.session_state.guess = st.session_state.widget
@@ -93,6 +172,7 @@ def new_round(mode):
     st.session_state.disable_guess_input = False
     st.session_state.disable_hint_btn = False
     st.session_state.disable_giveup_btn = False
+    st.session_state.game_over_msg = ""
 
     generated_lyrics = st.session_state.lyrics.generate(mode)
     correct_song = st.session_state.lyrics.get_track_name()
@@ -103,25 +183,25 @@ def new_round(mode):
     return generated_lyrics, correct_song, correct_album, next_line, prev_line
 
 def end_current_game(): 
-    accuracy_pct = round((st.session_state.correct_rounds_count / st.session_state.round_count), 2) * 100
-    possible_pct = (st.session_state.points / (st.session_state.round_count * points_mapping[mode])) * 100
+    if st.session_state.round_count:
+        accuracy_pct = round((st.session_state.correct_rounds_count / st.session_state.round_count), 2) * 100
+        possible_pct = (st.session_state.points / (st.session_state.round_count * points_mapping[mode])) * 100
 
-    st.session_state.past_game_scores = f"""
-**{mode_mapping[mode]} difficulty, {st.session_state.round_count} rounds played**
-* :dart: Accuracy: {st.session_state.correct_rounds_count}/{st.session_state.round_count} ({accuracy_pct}%)
-* :100: Points out of total possible: {st.session_state.points}/{st.session_state.round_count * points_mapping[mode]} ({round(possible_pct, 2)}%)
-* :fire: Max streak: {max(st.session_state.streaks) if len(st.session_state.streaks) else 0}
-* :moneybag: :green[**Total points: {st.session_state.points}**]
-"""
-
+        st.session_state.past_game_scores = f"""
+    {mode_mapping[mode]} difficulty, {st.session_state.round_count} rounds played
+    * :dart: Accuracy: {st.session_state.correct_rounds_count}/{st.session_state.round_count} ({accuracy_pct}%)
+    * :100: Points out of total possible: {st.session_state.points}/{st.session_state.round_count * points_mapping[mode]} ({round(possible_pct, 2)}%)
+    * :fire: Max streak: {max(st.session_state.streaks) if len(st.session_state.streaks) else 0}
+    * :moneybag: :green[Total points: {st.session_state.points}]
+    """
     st.session_state.streak = 0
     st.session_state.streaks = []
     st.session_state.disable_start_btn = False
-    st.session_state.disable_general = False
     st.session_state.show_lyrics = False
     st.session_state.points = 0
     st.session_state.round_count = 0
     st.session_state.correct_rounds_count = 0
+    st.session_state.guess = ""
 
 def disable_start_button(): 
 
@@ -129,7 +209,6 @@ def disable_start_button():
         start_form.error("Please select at least one album.")
     else: 
         st.session_state.disable_start_btn = True
-        st.session_state.disable_general = True
         st.session_state.lives = 3
 
         filtered_lyrics = all_lyrics[all_lyrics["album_name"].isin(st.session_state.albums)].reset_index()
@@ -151,12 +230,9 @@ def give_up(game_mode):
     if game_mode == "Survival (with 3 lives)":
         msg = f"The correct answer was {st.session_state.correct_song}, from the album {st.session_state.correct_album}. \n\nYou lost a life and have {st.session_state.lives} lives left."
         if st.session_state.lives == 0: 
-            container.error(f"**GAME OVER**: You ran out of lives! Please start a new game.", icon="😢")
-            # TODO: print correct answer befre ending game
-            container.error(msg, icon="🚨")
+            st.session_state.game_over_msg = f'**GAME OVER**: You ran out of lives! Please start a new game.\n\nThe correct answer was **{st.session_state.correct_song}**, from the album **{st.session_state.correct_album}**.'
             end_current_game()
             return
-    # TODO: it is showing the error twice - this stuff down here runs even if game over
     container.error(msg, icon="🚨")
     container.button(":arrow_right: Next round", on_click=next_round)
 
@@ -178,6 +254,7 @@ def answered_correctly():
     st.session_state.guess = None
     st.session_state.disable_hint_btn = True
     st.session_state.disable_giveup_btn = True
+    st.session_state.disable_guess_input = True
     st.session_state.streak += 1
 
     container.button(":arrow_right: Next round", on_click=next_round)
@@ -191,34 +268,42 @@ def answered_incorrectly(game_mode):
     if game_mode == "Survival (with 3 lives)":
         msg = msg + f"\n\nYou lost a life and have {st.session_state.lives} lives left."
         if st.session_state.lives == 0: 
-            container.error(f"**GAME OVER**: You ran out of lives! Please start a new game.", icon="😢")
-            # TODO: print correct answer befre ending game
-            container.error(f"The correct answer was {st.session_state.correct_song}, from the album {st.session_state.correct_album}", icon="🚨")
+            st.session_state.disable_guess_input = True
+            st.session_state.disable_giveup_btn = True
+            st.session_state.disable_hint_btn = True
+            st.session_state.game_over_msg = f'"{st.session_state.guess}" is not correct.\n\n**GAME OVER**: You ran out of lives! Please start a new game.\n\nThe correct answer was **{st.session_state.correct_song}**, from the album **{st.session_state.correct_album}**.'
             end_current_game()
+            st.rerun()
+            return
     container.error(msg, icon="🚨")
     
 
 st.title("Welcome to :sparkles:tayLyrics:sparkles:!")
 
 # SIDEBAR #
-sidebar = st.sidebar
-with sidebar: 
+with st.sidebar: 
     with st.expander(":pencil2: Instructions"): 
         st.markdown(f"Lyrics range from debut to *{all_albums[-1]}*.")
-        st.markdown(f':red[**IMPORTANT: do NOT include parentheses in your guesses; e.g. "Back to December (Taylor\'s Version)" should simply be "Back to December."**]')
+        st.markdown(f':red[IMPORTANT: do NOT include "(Taylor\'s Version)" in your guesses; e.g. "Back to December (Taylor\'s Version)" should simply be "Back to December."]')
+        st.markdown(f':red[IMPORTANT: answer "All Too Well" for BOTH the 5-minute and 10-minute versions of All Too Well.]')
+
+        selected_theme = st.radio("Select a theme",
+                                  options=themes)
+        apply_theme(theme_css[selected_theme])
+
     start_form = st.form("game_settings")
     with start_form:
         mode = st.selectbox("Select a game difficulty", 
                             options=mode_options, 
-                            disabled=st.session_state.disable_general)
+                            disabled=st.session_state.disable_start_btn)
         game_mode = st.selectbox("Select a game mode", 
                                  options=gamemode_options,
-                                 disabled=st.session_state.disable_general)
+                                 disabled=st.session_state.disable_start_btn)
         with st.expander("Advanced options"):
             selected_albums = st.multiselect("Select albums to generate lyrics from", 
                                              options=all_albums, 
                                              default=all_albums, 
-                                             disabled=st.session_state.disable_general, 
+                                             disabled=st.session_state.disable_start_btn, 
                                              # on_change=albums_selected, 
                                              key="albums")
         start_btn = st.form_submit_button(":large_green_square: Start new game", disabled=(st.session_state.disable_start_btn),
@@ -227,7 +312,6 @@ with sidebar:
 # MAIN PANEL #
 if (start_btn and len(selected_albums)) or st.session_state.next: 
     st.session_state.generated_lyrics, st.session_state.correct_song, st.session_state.correct_album, st.session_state.next_line, st.session_state.prev_line = new_round(mode=mode)
-    st.write(st.session_state.lyrics.rand_num)
 
 container = st.container(border=True)
 with container: 
@@ -247,25 +331,27 @@ with container:
                                help=hint_help)
         giveup_btn = st.button(":no_entry: Give up", on_click=give_up, disabled=st.session_state.disable_giveup_btn,
                                help="2 points are deducted from your total if you give up.", args=(game_mode,))
-        col3.button(":octagonal_sign: End current game", on_click=end_current_game)
+        col3.button(":octagonal_sign: End current game", on_click=end_current_game, key="end_game")
     else: 
         st.markdown('Click the "Start new game" button to start guessing!')
         if st.session_state.past_game_scores: 
             st.markdown("### Past Game Statistics")
             st.markdown(st.session_state.past_game_scores)
+        if st.session_state.game_over_msg:
+            st.error(st.session_state.game_over_msg, icon="😢")
 
 st.sidebar.divider()
 with st.sidebar.container(border=True):
     st.markdown("### Game Statistics")
-    # TODO: add the album filtering stuff; all if all, otherwise list all
     st.markdown(f"**{mode_mapping[mode]} difficulty, {game_mode} mode**")
+
     if st.session_state.round_count: 
-        st.markdown(f"* :large_green_circle: **Round: {st.session_state.round_count}**")
+        st.markdown(f"* :large_green_circle: Round: {st.session_state.round_count}")
         accuracy_pct = round((st.session_state.correct_rounds_count / st.session_state.round_count), 2) * 100
         st.markdown(f"* :dart: Accuracy: {st.session_state.correct_rounds_count}/{st.session_state.round_count} ({accuracy_pct}%)")
         possible_pct = (st.session_state.points / (st.session_state.round_count * points_mapping[mode])) * 100
         st.markdown(f"* :100: Points out of total possible: {st.session_state.points}/{st.session_state.round_count * points_mapping[mode]} ({round(possible_pct, 2)}%)")
         st.markdown(f"* :fire: Current streak: {st.session_state.streak}")
-        st.markdown(f"* :moneybag: :green[**Total points: {st.session_state.points}**]")
+        st.markdown(f"* :moneybag: :green[Total points: {st.session_state.points}]")
     if game_mode == "Survival (with 3 lives)":
-        st.markdown(f"* :red[Lives: {st.session_state.lives}]")
+        st.markdown(f"* :space_invader: :red[Lives: {st.session_state.lives}]")
