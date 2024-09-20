0c49e90a50e8efa09fb331220a5bfe1c0547dc33
jasminex21 <jasminexu7003@icloud.com>
2024-09-20 18:24:51 -0500

    renamed stats page

--- gui/tayLyrics.py
+++ gui/tayLyrics.py
@@ -541,7 +541,7 @@ with main_col:
     st.title("Welcome to :sparkles:tayLyrics:sparkles:!")
     if st.session_state.game_in_progress == False: 
 
-        start_tab, past_stats_tab, leaderboard_tab = st.tabs(["Start New Game", "Past Game Statistics", "Leaderboard"])
+        start_tab, past_stats_tab, leaderboard_tab = st.tabs(["Start New Game", "Stats", "Leaderboard"])
 
         with start_tab: 
             exp = st.expander(":pencil2: Instructions (click to expand)", expanded=False)
